6a01c250086084007ba47d38facd24d3c131df0d
jaeho0613 <ywm2004@naver.com>
2020-02-13 21:44:02 +0900

    [1부] Doage
    p330 : GameManager 스크립트 작성

---
+++ Unity_essence/Dodge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity_essence/Dodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    public float speed = 8f; // 이동 속력
+    
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.UpArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(0f, 0f, speed);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(0f, 0f, -speed);
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(speed, 0f, 0f);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow) == true)
+        //{
+        //    playerRigidbody.AddForce(-speed, 0f, 0f);
+        //}
+
+        // 수평축과 수직축의 입력값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        // 실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // Vecotr3 속도를 (xSpeed, 0, zSpeed) 로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        // 리지드바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
