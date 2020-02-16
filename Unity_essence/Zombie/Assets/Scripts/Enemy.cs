d8db960f8d0b263c75232daeea8f1a6b2d29e6df
jaeho0613 <ywm2004@naver.com>
2020-02-16 18:19:42 +0900

    20 - 02 - 16 [zombie] [완성]
    - 싱글 플레이 zombie game 완성
    - p792

--- Unity_essence/Zombie/Assets/Scripts/Enemy.cs
+++ Unity_essence/Zombie/Assets/Scripts/Enemy.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드를 가져오기
 
 // 적 AI를 구현한다
-public class Enemy : LivingEntity {
+public class Enemy : LivingEntity
+{
     public LayerMask whatIsTarget; // 추적 대상 레이어
 
     private LivingEntity targetEntity; // 추적할 대상
@@ -37,47 +38,153 @@ public class Enemy : LivingEntity {
         }
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         // 초기화
+        pathFinder = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAudioPlayer = GetComponent<AudioSource>();
+
+        // 렌더러 컴포넌트는 자식 게임 오브젝트에 있으므로
+        // GetComponentInChildren() 메서드 사용
+        enemyRenderer = GetComponentInChildren<Renderer>();
     }
 
     // 적 AI의 초기 스펙을 결정하는 셋업 메서드
-    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor) {
+    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor)
+    {
+        // 체력 설정
+        startingHealth = newHealth;
+        health = newHealth;
+
+        // 공격력 설정
+        damage = newDamage;
+        // 내비메시 에이전트의 이동 속도 설정
+        pathFinder.speed = newSpeed;
+        // 렌더러가 사용 중인 머티리얼의 컬러를 변경, 외형 색이 변함
+        enemyRenderer.material.color = skinColor;
     }
 
-    private void Start() {
+    private void Start()
+    {
         // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
         StartCoroutine(UpdatePath());
     }
 
-    private void Update() {
+    private void Update()
+    {
         // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
         enemyAnimator.SetBool("HasTarget", hasTarget);
     }
 
     // 주기적으로 추적할 대상의 위치를 찾아 경로를 갱신
-    private IEnumerator UpdatePath() {
+    private IEnumerator UpdatePath()
+    {
         // 살아있는 동안 무한 루프
         while (!dead)
         {
+            if (hasTarget)
+            {
+                // 추적 대상 존재 : 경로를 갱신하고 AI 이동을 계속 진행
+                pathFinder.isStopped = false;
+                pathFinder.SetDestination(
+                    targetEntity.transform.position);
+            }
+            else
+            {
+                // 추적 대상 없음 : AI 이동 중지
+                pathFinder.isStopped = true;
+
+                // 20유닛의 반지름을 가진 가상의 구를 그렸을 때 구와 겹치는 모든 콜라이더를 가져옴
+                // 단, whatIsTarget 레이어를 가진 콜라이더만 가져오도록 필터링
+                Collider[] colliders =
+                    Physics.OverlapSphere(transform.position, 20f, whatIsTarget);
+
+                // 모든 콜라이더를 순회하면서 살아 있는 LivingEntity 찾기
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    // 콜라이더로부터 LivingEntity 컴포넌트 가져오기
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+
+                    // LivingEntity 컴포넌트가 존재하며, 해당 LivingEntity가 살아 있다면
+                    if (livingEntity != null && !livingEntity.dead)
+                    {
+                        // 추적 대상을 해당 LivingEntity로 설정
+                        targetEntity = livingEntity;
+
+                        // for 문 루프 즉시 정지
+                        break;
+                    }
+                }
+            }
             // 0.25초 주기로 처리 반복
             yield return new WaitForSeconds(0.25f);
         }
     }
 
     // 데미지를 입었을때 실행할 처리
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        // 아직 사망하지 않은 경우메나 피격 효과 재생
+        if (!dead)
+        {
+            // 공격받은 지점과 방향으로 파티클 효과 재생
+            hitEffect.transform.position = hitPoint;
+            hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+            hitEffect.Play();
+
+            // 피격 효과음 재생
+            enemyAudioPlayer.PlayOneShot(hitSound);
+        }
         // LivingEntity의 OnDamage()를 실행하여 데미지 적용
         base.OnDamage(damage, hitPoint, hitNormal);
     }
 
     // 사망 처리
-    public override void Die() {
+    public override void Die()
+    {
         // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
         base.Die();
+
+        // 다른 AI를 방해하지 않도록 자신의 모든 콜라이더를 비활성화
+        Collider[] enemyColliders = GetComponents<Collider>();
+        for (int i = 0; i < enemyColliders.Length; i++)
+        {
+            enemyColliders[i].enabled = false;
+        }
+
+        // AI 추적을 중지하고 내비메시 컴포넌트 비활성화
+        pathFinder.isStopped = true;
+        pathFinder.enabled = false;
+
+        // 사망 애니메이션 재생
+        enemyAnimator.SetTrigger("Die");
+        // 사망 효과음 재생
+        enemyAudioPlayer.PlayOneShot(deathSound);
     }
 
-    private void OnTriggerStay(Collider other) {
-        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행   
+    private void OnTriggerStay(Collider other)
+    {
+        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행 
+        // 자신이 사망하지 않았으며,
+        // 최근 공격 시점에서 timeBetAttack 이상 지났다면 공격 가능
+        if(!dead && Time.time >= lastAttackTime + timeBetAttack)
+        {
+            // 상대방의 LivingEntity 타입 가져오기 시도
+            LivingEntity attackTarget = other.GetComponent<LivingEntity>();
+
+            // 상대방의 LivingEntity가 추적 대상이라면 공격 실행
+            if(attackTarget != null && attackTarget == targetEntity)
+            {
+                // 최근 공격 시간 갱신;
+                lastAttackTime = Time.time;
+                // 상대방의 피격 위치와 피격 방향을 근삿값으로 계산
+                Vector3 hitPoint = other.ClosestPoint(transform.position);
+                Vector3 hitNormal = transform.position - other.transform.position;
+
+                // 공격 실행
+                attackTarget.OnDamage(damage, hitPoint, hitNormal);
+            }
+        }
     }
 }
--- Unity_essence/Zombie/Assets/Scripts/EnemySpawner.cs
+++ Unity_essence/Zombie/Assets/Scripts/EnemySpawner.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 
 // 적 게임 오브젝트를 주기적으로 생성
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawner : MonoBehaviour
+{
     public Enemy enemyPrefab; // 생성할 적 AI
 
     public Transform[] spawnPoints; // 적 AI를 소환할 위치들
@@ -21,7 +22,8 @@ public class EnemySpawner : MonoBehaviour {
     private List<Enemy> enemies = new List<Enemy>(); // 생성된 적들을 담는 리스트
     private int wave; // 현재 웨이브
 
-    private void Update() {
+    private void Update()
+    {
         // 게임 오버 상태일때는 생성하지 않음
         if (GameManager.instance != null && GameManager.instance.isGameover)
         {
@@ -39,16 +41,61 @@ public class EnemySpawner : MonoBehaviour {
     }
 
     // 웨이브 정보를 UI로 표시
-    private void UpdateUI() {
+    private void UpdateUI()
+    {
         // 현재 웨이브와 남은 적의 수 표시
         UIManager.instance.UpdateWaveText(wave, enemies.Count);
     }
 
     // 현재 웨이브에 맞춰 적을 생성
-    private void SpawnWave() {
+    private void SpawnWave()
+    {
+        // 웨이브 1 증가
+        wave++;
+
+        // 현제 웨이브 * 1.5를 반올림한 수만큼 적 생성
+        int spawnCount = Mathf.RoundToInt(wave * 1.5f);
+
+        // spawnCount만큼 적 생성
+        for (int i = 0; i < spawnCount; i++)
+        {
+            // 적의 세기를 0%에서 100% 사이에서 랜덤 결정
+            float enemyIntensity = Random.Range(0f, 1f);
+            // 적 생성 처리 실행
+            CreateEnemy(enemyIntensity);
+        }
     }
 
     // 적을 생성하고 생성한 적에게 추적할 대상을 할당
-    private void CreateEnemy(float intensity) {
+    private void CreateEnemy(float intensity)
+    {
+        // intensity를 기반으로 적의 능력치 결정
+        float health = Mathf.Lerp(healthMin, healthMax, intensity);
+        float damage = Mathf.Lerp(damageMin, damageMax, intensity);
+        float speed = Mathf.Lerp(speedMin, speedMax, intensity);
+
+        // intensity를 기반으로 하얀색과 enemyStrength 사이에서 적의 피부색 결정
+        Color skinColor = Color.Lerp(Color.white, strongEnemyColor, intensity);
+
+        // 생성할 위치를 랜덤으로 결정
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        // 적 프리팹으로부터 적 생성
+        Enemy enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        // 생성한 적의 능력치와 추적 대상 설정
+        enemy.Setup(health, damage, speed, skinColor);
+
+        // 생성된 적을 리스트에 추가
+        enemies.Add(enemy);
+
+        // 적의 OnDeath 이벤트에 익명 메서드 등록
+        // 사망한 적을 리스트에서 제거
+        enemy.onDeath += () => enemies.Remove(enemy);
+        // 사망한 적을 10초 뒤에 파괴
+        enemy.onDeath += () => Destroy(enemy.gameObject, 10f);
+        // 적 사망 시 점수 상승
+        enemy.onDeath += () => GameManager.instance.AddScore(100);
+
     }
 }
