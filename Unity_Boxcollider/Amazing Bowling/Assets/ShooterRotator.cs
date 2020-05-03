ab7b42cf1bb00ce06a15ec748fd336291af26898
Jaeho <49357353+jeaho0613@users.noreply.github.com>
2020-05-03 20:20:11 +0900

    Readme 참고

---
+++ Unity_Boxcollider/Amazing Bowling/Assets/ShooterRotator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterRotator : MonoBehaviour
+{
+    private enum RotateState
+    {
+        Idle,
+        Vertical,
+        Horizontal,
+        Ready
+    }
+
+    private RotateState state = RotateState.Idle;
+
+    public float verticalRotateSpeed = 360f;
+    public float horizontalRotateSpeed = 360f;
+    public BallShooter ballShooter;
+    private void Update()
+    {
+        switch (state)
+        {
+            case RotateState.Idle:
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    state = RotateState.Horizontal;
+                }
+                break;
+
+            case RotateState.Horizontal:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Vertical;
+                }
+                break;
+
+            case RotateState.Vertical:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Ready;
+                    ballShooter.enabled = true;
+                }
+                break;
+
+            case RotateState.Ready:
+                break;
+        }
+    }
+
+    private void OnEnable()
+    {
+        transform.rotation = Quaternion.identity;
+        state = RotateState.Idle;
+        ballShooter.enabled = false;
+    }
+}
